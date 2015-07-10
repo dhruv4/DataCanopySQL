ad5c2f4880dc2c5274b956ebc4917f5dff9d3a46
Dhruv Gupta <dhruv4@gmail.com>
2015-07-10 14:27:52 -0400

    Just checking

--- testCache.py
+++ testCache.py
@@ -64,6 +64,7 @@ def runExperiment():
 	numStats = Config.getint("Data Canopy Config", "NumStats")
 	numCols = Config.getint("Data Set Config", "NumCols")
 	numLevels = Config.getint("Data Canopy Config", "NumLevels")
+	xaxis = Config.get("Experiment Config", "XAxis")
 
 	times = []
 	vals = []
