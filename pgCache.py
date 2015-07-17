1440eccaddecf3b570a1afe99e4774abd6875b33
Dhruv Gupta <dhruv4@gmail.com>
2015-07-17 13:29:45 -0400

    test

--- pgCache.py
+++ pgCache.py
@@ -227,14 +227,14 @@ def createDCTableLeveln(table, levels, numChunks, numCols, numRows, two = 0):
 	for i in range(3, levels+1):
 		print("reached", i)
 		comb = list(itertools.combinations(range(1, numCols + 1), i))
-		for j in comb:
-			if(two == 1):
-				comb2 = list(itertools.combinations(j, 2))
-			else:
-				comb2 = list(itertools.combinations(j, i-1))
-			
-			for cval in range(numChunks):
-				vals = []	
+		for cval in range(numChunks):
+			for j in comb:
+				for cval in range(numChunks):
+				vals = []
+				if(two == 1):
+					comb2 = list(itertools.combinations(j, 2))
+				else:
+					comb2 = list(itertools.combinations(j, i-1))
 				for k in comb2:
 					cur.execute("SELECT col1 FROM dc_" + table + " WHERE col0 = cast('" 
 						+ recToBinTrans(k, cval, numCols, numChunks) + "' as varbit)")
