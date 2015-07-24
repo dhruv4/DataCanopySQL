d31149c22faebd7365f9922f06a163092abe1b19
gupta <gupta@adama.seas.harvard.edu>
2015-07-23 22:09:53 -0400

    load from csv

--- createFiles.py
+++ createFiles.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-#for x in [10000, 100000, 1000000, 1000000, 100000000]:
-for x in [1000000, 10000000, 100000000]:
-	np.save("test" + str(x) + ".npy", np.random.rand(x, 15))
-	print("made x")
+for x in [10000, 100000, 1000000, 1000000, 100000000]:
+#for x in [1000000, 10000000, 100000000]:
+	#np.save("test" + str(x) + ".npy", np.random.rand(x, 15))
+	np.savetxt("test" + str(x) + ".csv", np.random.rand(x, 15),delimiter=",")
+	print("made", x)
 
