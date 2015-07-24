36f9bc9ba83c0aacec260d79823d963f650a3d0e
gupta <gupta@adama.seas.harvard.edu>
2015-07-23 22:01:09 -0400

    checking gitignore

--- createFiles.py
+++ createFiles.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #for x in [10000, 100000, 1000000, 1000000, 100000000]:
-for x in [100000000]:
+for x in [1000000, 10000000, 100000000]:
 	np.save("test" + str(x) + ".npy", np.random.rand(x, 15))
 	print("made x")
 
