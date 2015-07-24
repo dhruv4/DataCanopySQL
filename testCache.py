f90a9e95e005538ac85a182cf504e1a5bdbf0985
Dhruv Gupta <dhruv4@gmail.com>
2015-07-24 10:41:55 -0400

    testing copy

--- testCache.py
+++ testCache.py
@@ -112,7 +112,8 @@ def runExperiment():
 			cur = conn.cursor()
 			pgCache.createTable(cur, conn, 'exp', numCols + 1)
 			#pgCache.insertRandData(cur, conn, 'exp', numRows)
-			cur.execute("COPY exp FROM 'test" + str(numRows) + ".csv' DELIMITER ',' CSV")
+			#cur.execute("COPY exp FROM 'test" + str(numRows) + ".csv' DELIMITER ',' CSV")
+			cur.execute("COPY exp FROM 'test" + str(numRows) + "WITH BINARY")
 
 		elif(sys.argv[1] == "mdb"):
 
@@ -120,8 +121,8 @@ def runExperiment():
 			cur = conn.cursor()
 			mdbCache.createTable(cur, conn, 'exp', numCols + 1)
 			#mdbCache.insertRandData(cur, conn, 'exp', numRows)
-			cur.execute("COPY INTO exp FROM 'test" + str(numRows) + ".csv' DELIMITER ',' CSV")
-
+			#cur.execute("COPY INTO exp FROM 'test" + str(numRows) + ".csv' DELIMITER ',' CSV")
+			cur.execute("COPY INTO exp FROM 'test" + str(numRows) + ".npy' ")
 		
 		conn.commit()
 
