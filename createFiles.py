dea622c691c9486afa7a6be9f96ea079fd1dde3d
Dhruv Gupta <dhruv4@gmail.com>
2015-07-26 01:31:00 -0400

    fix createFiles

--- createFiles.py
+++ createFiles.py
@@ -3,6 +3,7 @@ import numpy as np
 #for x in [10000, 100000, 1000000, 10000000, 100000000]:
 for x in [5, 10, 15, 20, 25]:
 	#np.save("test" + str(x) + ".npy", np.random.rand(x, 15))
-	np.savetxt("test" + str(x) + ".csv", np.random.rand(15, 100000),delimiter=",")
+	#np.savetxt("test" + str(x) + ".csv", np.random.rand(15, 100000),delimiter=",")
+	np.savetxt("test" + str(x) + ".csv", np.random.rand(x, 100000),delimiter=",")
 	print("made", x)
 
