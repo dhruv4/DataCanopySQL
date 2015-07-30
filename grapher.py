c71c7e70f78311ac41be59fa07871dd60ed6d5ed
Dhruv Gupta <dhruv4@gmail.com>
2015-07-30 16:36:31 -0400

    comparative graphs made

--- grapher.py
+++ grapher.py
@@ -53,15 +53,15 @@ def graph(xtitle, name, level, db, c=0, ylog=0):
 	if(ylog == 1):
 		if(c == 1):
 			plt.yscale('log')
-			plt.savefig(db + 'results/graphs/mpl_' + db + '_cache_' + name + xtitle + 'log.pdf')
+			plt.savefig(db + 'results/graphs/' + db + '_cache_' + name + xtitle + 'log.pdf')
 		else:
 			plt.yscale('log')
-			plt.savefig(db + 'results/graphs/mpl_' + db + '_time_' + name + xtitle + 'log.pdf')
+			plt.savefig(db + 'results/graphs/' + db + '_time_' + name + xtitle + 'log.pdf')
 	else:
 		if(c == 1):
-			plt.savefig(db + 'results/graphs/mpl_' + db + '_cache_' + name + xtitle + '.pdf')
+			plt.savefig(db + 'results/graphs/' + db + '_cache_' + name + xtitle + '.pdf')
 		else:
-			plt.savefig(db + 'results/graphs/mpl_' + db + '_time_' + name + xtitle + '.pdf')
+			plt.savefig(db + 'results/graphs/' + db + '_time_' + name + xtitle + '.pdf')
 
 
 def main():
@@ -105,6 +105,100 @@ def main():
 	plt.close()
 
 
-#plt = matplotlib.pyplot
+#main()
 
-main()
+def cgraph(xtitle, name, db, c=0, ylog=0):
+
+	if not os.path.exists("results/graphs"):
+		os.makedirs("results/graphs")
+
+	if(c == 1):
+		f = open(db + 'results/' + name + 'total' + '_' + db + '_cache_' + xtitle +  '.txt', 'r')
+	else:
+		f = open(db + 'results/' + name + 'total' + '_' + db + '_time_' + xtitle +  '.txt', 'r')
+
+	x = []
+	t = []
+
+	for line in f:
+		nums = line.split(',')
+		x.append(nums[0])
+		t.append(nums[1][:-1])
+
+	print("x", x, "t", t, name, db)
+
+	if(db =="dc"):
+		plt.plot(x, t, '-yo', label="Data Canopy System")
+	if(db =="pg"):
+		plt.plot(x, t, '-r+', label="Postgres")
+	if(db =="mdb"):
+		plt.plot(x, t, '-gx', label="MonetDB")
+
+	plt.legend(loc="best")
+	if(xtitle == "Rows" or xtitle == "Chunks"):
+		plt.xscale('log')
+
+	if(c == 1):	
+		plt.title(xtitle + " vs Cache Misses")
+		plt.ylabel('Cache Misses')
+	else:
+		plt.title(xtitle + " vs Time (sec)")
+		plt.ylabel('Time (Sec)')
+	plt.tight_layout()
+	plt.xlabel(xtitle)
+	
+	if(ylog == 1):
+		if(c == 1):
+			plt.yscale('log')
+			plt.savefig('results/graphs/compare_cache_' + name + xtitle + 'log.pdf')
+		else:
+			plt.yscale('log')
+			plt.savefig('results/graphs/compare_time_' + name + xtitle + 'log.pdf')
+	else:
+		if(c == 1):
+			plt.savefig('results/graphs/compare_cache_' + name + xtitle + '.pdf')
+		else:
+			plt.savefig('results/graphs/compare_time_' + name + xtitle + '.pdf')
+
+def compare():
+
+	#arg1 = Test, Test2
+	#arg2 = Rows, Cols, Chunks
+
+	levels = ['dc', 'pg', 'mdb']
+
+	for j in levels:
+
+		cgraph(sys.argv[2], sys.argv[1], j, 1)
+
+	print("graphed cache")
+
+	plt.close()
+
+	for j in levels:
+
+		cgraph(sys.argv[2], sys.argv[1], j, 1, 1)
+	
+	print("graphed cache with log")
+
+	plt.close()
+
+	for j in levels:
+
+		cgraph(sys.argv[2], sys.argv[1], j, 0)
+
+	print("graphed time")
+
+	plt.close()
+
+	for j in levels:
+
+		cgraph(sys.argv[2], sys.argv[1], j, 0, 1)
+	
+	print("graphed time with log")
+
+	plt.close()
+
+compare()
+
+
